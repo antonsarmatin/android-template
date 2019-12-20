626939bfcca05f951d9f313a4a5e9c0cbffef5c7
anton.sarmatin <anton.sarmatin@codezavod.ru>
2019-12-20 10:01:17 +0300

    base failure handler

--- app/src/main/java/ru/sarmatin/template/presentation/platform/BaseFragment.kt
+++ app/src/main/java/ru/sarmatin/template/presentation/platform/BaseFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import ru.sarmatin.template.App
+import ru.sarmatin.template.core.exception.Failure
 import ru.sarmatin.template.di.component.AppComponent
 import javax.inject.Inject
 
@@ -19,8 +22,20 @@ abstract class BaseFragment : Fragment() {
 
     abstract fun layoutId(): Int
 
+    abstract fun handleFailure(): LiveData<Failure>?
+
     val appComponent: AppComponent by lazy(mode = LazyThreadSafetyMode.NONE) {
         (activity?.application as App).appComponent
+    }
+
+    private val failureObserver by lazy {
+        Observer<Failure> {
+        when(it){
+            is Failure.FeatureFailure -> TODO()
+            Failure.NetworkConnection -> TODO()
+            Failure.ServerError -> TODO()
+        }
+    }
     }
 
     @Inject
@@ -28,6 +43,11 @@ abstract class BaseFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(layoutId(), container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleFailure()?.observe(viewLifecycleOwner, failureObserver)
+    }
 
     open fun onBackPressed() {}
 
--- app/src/main/java/ru/sarmatin/template/presentation/ui/main/splash/SplashFragment.kt
+++ app/src/main/java/ru/sarmatin/template/presentation/ui/main/splash/SplashFragment.kt
@@ -1,11 +1,17 @@
 package ru.sarmatin.template.presentation.ui.main.splash
 
+import androidx.lifecycle.LiveData
 import ru.sarmatin.template.R
+import ru.sarmatin.template.core.exception.Failure
 import ru.sarmatin.template.presentation.platform.BaseFragment
 
 class SplashFragment : BaseFragment() {
 
     override fun layoutId() = R.layout.splash_fragment
+
+    override fun handleFailure(): LiveData<Failure>? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     companion object {
         fun newInstance() = SplashFragment()
