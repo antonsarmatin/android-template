4bf1d143a3e2c5109bafa27cb5f7ed9d6ca21fdd
anton.sarmatin <anton.sarmatin@codezavod.ru>
2019-12-28 17:07:50 +0300

    main activity extends base activity

--- app/src/main/java/ru/sarmatin/template/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/ru/sarmatin/template/presentation/ui/main/MainActivity.kt
@@ -3,8 +3,9 @@ package ru.sarmatin.template.presentation.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.sarmatin.template.R
+import ru.sarmatin.template.presentation.platform.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
